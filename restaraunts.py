4baa4ecd6974f1b9bf8c995bf1810b99d19ef594
Justin Johnson <jjohnson@ourfamilywizard.com>
2018-03-19 23:09:29 -0500

    Implemented user interaction for api call. Also seperated ui from program logic

--- restaraunts.py
+++ restaraunts.py
@@ -1,24 +1,36 @@
+# restaurant api caller
+# restaraunts.py
 import requests
+import ui
 
 # api key for zomato
 zomato_api = "57ca188bd1cb406464affeaa2690bb55"
-# base url for requests
-base_url = "https://developers.zomato.com/api/v2.1/search?"
-# city name variable
-city = "Twin Cities"
-# entity_id var that correlates to twin cities location
-entity_id = "826"
-
-restaraunts = "https://developers.zomato.com/api/v2.1/search?entity_id=826&entity_type=city"
-
+# base url for requests in the Twin Cities area
+base_url = "https://developers.zomato.com/api/v2.1/search?entity_id=826&entity_type=city"
+# header for api request
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": zomato_api}
 
-response = requests.get(restaraunts, headers=header).json()
+def print_restaraunt(start):
+    url = base_url + "&start=" + str(start) + "&count=1"
+    response = requests.get(url, headers=header).json()
+    # hopping through the json response and printing the necessary information
+    for item in response["restaurants"]:
+        r = item["restaurant"]
+        location = r["location"]
+        ui.message("\nName: " + r["name"])
+        ui.message("Tags: " + r["cuisines"])
+        ui.message("$ for 2: " + str(r["average_cost_for_two"]) + "$")
+        ui.message("Address: " + location["address"] + "\n")
 
-print(response["results_found"])
-print(response["results_start"])
-print(response["results_shown"])
 
-for item in response["restaurants"]:
-    r = item["restaurant"]
-    print(r["name"])
+def main():
+    start = 0
+    choice = "y"
+    ui.message("\n*Restaraunts in the Twin Cities Area*\n")
+    while choice != "n":
+        print_restaraunt(start)
+        choice = ui.prompt_for_more()
+        start+=1
+    ui.message("\nGoodbye!\n")
+
+main()
---
+++ ui.py
@@ -0,0 +1,10 @@
+# restaurant api caller
+# ui.py
+
+def prompt_for_more():
+    choice = input("Want info on another restaurant? (y/n))")
+    return choice
+
+def message(msg):
+    # get message from somewhere else in project, print to user
+    print(msg)
