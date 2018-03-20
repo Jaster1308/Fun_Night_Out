54be710c35f4398d75f427c51bc1f06d236a893e
Justin Johnson <jjohnson@ourfamilywizard.com>
2018-03-19 21:50:36 -0500

    Implemented a more organized API call

--- restaraunts.py
+++ restaraunts.py
@@ -2,6 +2,8 @@ import requests
 
 # api key for zomato
 zomato_api = "57ca188bd1cb406464affeaa2690bb55"
+# base url for requests
+base_url = "https://developers.zomato.com/api/v2.1/search?"
 # city name variable
 city = "Twin Cities"
 # entity_id var that correlates to twin cities location
@@ -11,6 +13,12 @@ restaraunts = "https://developers.zomato.com/api/v2.1/search?entity_id=826&entit
 
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": zomato_api}
 
-response = requests.get(restaraunts, headers=header)
+response = requests.get(restaraunts, headers=header).json()
 
-print(response.json())
+print(response["results_found"])
+print(response["results_start"])
+print(response["results_shown"])
+
+for item in response["restaurants"]:
+    r = item["restaurant"]
+    print(r["name"])
