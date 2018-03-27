f0957e3ba13ed0856cc1872e6c138cad24f5fc5d
Justin Johnson <jjohnson@ourfamilywizard.com>
2018-03-26 23:58:07 -0500

    Added testing for api calling with a invalid api key

--- fun_night_out_tests.py
+++ fun_night_out_tests.py
@@ -4,7 +4,8 @@ import os, requests, nose, restaurants
 
 # using third party nose library for mock api testing
 
-def test_restaurant_api():
+# test for restaurant api call with a valid api key
+def test_restaurant_api_good_key():
 
     # base information needed for restaurant api call
     zomato_api = os.environ.get("FOOD_KEY")
@@ -18,7 +19,23 @@ def test_restaurant_api():
     # test the Response
     nose.tools.assert_true(response.ok)
 
-def test_movie_api():
+# test for restaurant api call with a fake api key
+def test_restaurant_api_bad_key():
+
+    # base information needed for restaurant api call, with fake key
+    zomato_api = "12345"
+    base_url = "https://developers.zomato.com/api/v2.1/search?entity_id=826&entity_type=city"
+    header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": zomato_api}
+    url = base_url + "&start=1&count=1"
+
+    # make request for test
+    response = requests.get(url, headers=header)
+
+    # test the Response
+    nose.tools.assert_false(response.ok)
+
+# test for movie api call with a valid api key
+def test_movie_api_good_key():
 
     # base information needed for movie api call
     url = "https://api.themoviedb.org/3/movie/now_playing"
@@ -32,6 +49,20 @@ def test_movie_api():
     # test the response
     nose.tools.assert_true(response.ok)
 
+# test for movie api call with a fake api key
+def test_movie_api_bad_key():
+
+    # base information needed for movie api call, with fake key
+    url = "https://api.themoviedb.org/3/movie/now_playing"
+    key = "12345"
+    country = 'US'
+    payload = {'api_key' : key, 'region' : country}
+
+    # make request for test
+    response = requests.get(url, payload)
+
+    # test the response
+    nose.tools.assert_false(response.ok)
 
 if __name__ == '__main__':
     nose.main()
